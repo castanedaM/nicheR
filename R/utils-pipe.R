857f4e0612d124b9d1c816781e937bd35baf9da9
Mariana Castaneda-Guzman <mcguzman1997@gmail.com>
2025-10-19 19:11:09 -0400

    edits of knitting

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,22 +1,37 @@
 #' Internal imports for NicheR
 #'
-#' These imports are centralized here to keep roxygen/NAMESPACE tidy.
+#' Centralized roxygen imports to keep NAMESPACE tidy.
 #' Nothing in this file is exported.
 #'
 #' @keywords internal
-#'
+#' @name NicheR-internal-imports
+NULL
+
+# ---- Core plotting ----
 #' @import ggplot2
+NULL
+
+# dplyr verbs used unqualified
 #' @importFrom dplyr mutate filter row_number
+NULL
+
+# pipe and pronoun
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
+NULL
+
+# palettes, arranging, basemap, sf
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom ggpubr ggarrange
-#' @importFrom plotly plot_ly add_trace add_markers layout
 #' @importFrom sf st_as_sf
-#' @importFrom terra ncell as.data.frame ext nlyr xmin xmax ymin ymax crop
+NULL
+
+# terra/raster utilities (keep comments on their own line!)
+#' @importFrom terra ncell as.data.frame ext nlyr xmin xmax ymin ymax crop yFromRow
 #' @importFrom raster ncell extent
-#'
-#' # Database and SQL utilities used in convert_large_raster()
+NULL
+
+# Database and SQL utilities used in convert_large_raster()
 #' @importFrom DBI dbConnect dbDisconnect dbWriteTable dbReadTable
 #' @importFrom RSQLite SQLite
 NULL
